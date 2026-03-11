e49e6ba619b0763c61094065d8f636cec30dec31
Rabby716 <rabby716islam@gmail.com>
2026-03-11 07:49:18 +0600

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T01:49:14.498Z
-Commit: Add multi-language support
+Timestamp: 2026-03-11T01:49:31.971Z
+Commit: Resolve null pointer exception in user service
