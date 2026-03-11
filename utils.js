58490eeb5df0b6bfe74721f2a68b5503bb808b87
Rabby716 <rabby716islam@gmail.com>
2026-03-11 07:50:38 +0600

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T01:49:50.810Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-11T01:50:51.082Z
+Commit: Resolve null pointer exception in user service
